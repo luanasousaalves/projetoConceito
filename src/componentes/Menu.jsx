2657c360fb246a93d1f8e04c3bdfb92e93737675
Luana Sousa <luanaalvess2000@gmail.com>
2022-03-22 22:27:15 -0300

    commit incial

---
+++ src/componentes/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+
+       <ul>
+           <li><a href="">Pagina 1</a></li>
+           <li><a href="">Pagina 2</a></li>
+           <li><a href="">Pagina 3</a></li>
+       </ul>
+       
+    </div>
+  )
+}
+
+export default Menu
